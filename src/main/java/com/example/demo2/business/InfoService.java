ca00ce3307387daca023cae63f8d80fde2633fb7
Lucas L de Morais <lucasldemorais@gmail.com>
2022-09-13 13:24:07 -0300

    Ajustes e app properties

--- src/main/java/com/example/demo2/business/InfoService.java
+++ src/main/java/com/example/demo2/business/InfoService.java
@@ -1,18 +1,25 @@
 package com.example.demo2.business;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
 import io.github.bucket4j.Refill;
 
 @Service
 public class InfoService {
+
+    @Value("${ratelimit.auth.time}")private int time;
+    @Value("${ratelimit.auth.unit}")private String unit;
+    @Value("${ratelimit.auth.requestAmount}")private int requestAmount;
 
     private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
 
@@ -31,12 +38,13 @@ public class InfoService {
         }
         return remoteAddr;
     }
+
     Bandwidth getLimit() {
-        return Bandwidth.classic(2, Refill.intervally(1, Duration.ofSeconds(10)));
+        return Bandwidth.classic(requestAmount, Refill.intervally(1, Duration.of(time, ChronoUnit.valueOf(unit))));
     }
 
     public Bucket newBucket(String clientIp) {
-        return Bucket.builder()
+        return Bucket4j.builder()
             .addLimit(getLimit())
             .build();
     }
